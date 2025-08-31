edfe4f0b1fd26fbe1aa5d96f1ee537e48242066f
fortrainees2020 <64248056+fortrainees2020@users.noreply.github.com>
2025-08-31 14:58:29 +0530

    Add files via upload

---
+++ microservices/02-spring3-apigateway-service/src/test/java/com/cg/apigateway_service/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.apigateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
